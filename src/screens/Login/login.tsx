daa91a0fbff294838f1fea217908f59d30bc885f
operationd8979 <operationddd@gmail.com>
2023-08-03 16:45:26 +0700

    css login-page 0.7

--- src/screens/Login/login.tsx
+++ src/screens/Login/login.tsx
@@ -1,19 +1,40 @@
 import React from "react";
 
 const Login = () => {
+
     return (
         <div className="login-screen">
-            <div className="row">
-                <div className="col-6 wrap-left background-login">
-                    <p className="description white">Nice to meet you from TIPCONNECT</p>
-                    <p className="title white">WELCOME BACK</p>
-                </div>
-                <div className="col-6 wrap-right">
-                    <p>Right</p>
-                    <p>WELCOME BACK</p>
+            <div className="background-flur"/>
+            <div className="login-section sadow">
+                <div className="row shadow h-100">
+                    <div className="col-6 wrap-left center background-left">
+                        <p className="description white">Nice to meet you from TIPCONNECT</p>
+                        <p className="title white">WELCOME BACK</p>
+                    </div>
+                    <div className="col-6 wrap-right center">
+                        <p className="title white">Login Account</p>
+                        <form className="form-login">
+                            <div className="form-outline mb-4">
+                                <label className="lable">User</label>
+                                <input placeholder="username"></input>
+                            </div>
+                            <div className="form-outline mb-4">
+                                <label className="lable">Password</label>
+                                <input placeholder="password"></input>
+                            </div>
+                            <div className="form-outline mb-4">
+                                <button className="button-login">LOGIN</button>
+                            </div>
+                        </form>
+                        <div className="row center">
+                            <p className="mb-4">Don't have an account?</p>
+                            <a className="herf white">CREATE NEW</a>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
+
     )
 }
 
