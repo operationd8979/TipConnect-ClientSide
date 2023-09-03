9dc9d4649d554bc93123baf55bfcdd74c64b492c
operationd8979 <operationddd@gmail.com>
2023-09-03 15:21:17 +0700

    change onlyHeader layout

--- src/components/Layout/OnlyHeader/index.tsx
+++ src/components/Layout/OnlyHeader/index.tsx
@@ -1,16 +1,14 @@
 import { Header } from '../components';
-import Sidebar from './Sidebar';
 
-function DefaultLayout({ children }: { children: JSX.Element }) {
+const OnlyHeader = ({ children }: { children: JSX.Element }) => {
     return (
         <div>
             <Header />
             <div className="container">
-                <Sidebar />
                 <div className="content">{children}</div>
             </div>
         </div>
     );
-}
+};
 
-export default DefaultLayout;
+export default OnlyHeader;
